190514449aa4ee21945154ee5b4d2cf2244b424c
Silentiy <a.gwrlnk@gmail.com>
2022-09-14 00:49:21 +0300

    "student_list and student_personal with according views and urls"

--- beam/autograder/views.py
+++ beam/autograder/views.py
@@ -15,3 +15,26 @@ class GroupList(generic.ListView):
     template_name = "RC_beam/group_list.html"
     context_object_name = "group_list"
     # there is no need to specify two above variables; Django uses them by default
+
+
+class StudentList(generic.ListView):
+    model = Student
+
+    def get_group_id(self):
+        return self.kwargs['group_id']
+
+    def get_queryset(self):
+        """ Return a list of students from group with given 'group_id' """
+        group_id = self.get_group_id()
+        return Student.objects.filter(group__id=group_id)
+
+    def get_context_data(self, **kwargs):
+        group_id = self.get_group_id()
+        context = super(StudentList, self).get_context_data(**kwargs)
+        context["group_id"] = group_id
+        context["group_name"] = Group.objects.filter(pk=group_id)[0]
+        return context
+
+
+def student_personal_view(request, group_id, stud_id):
+    return HttpResponse(f"Hello, {stud_id} from {group_id}")
