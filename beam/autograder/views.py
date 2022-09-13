067dfd497222b0d70b82aa1b4499c1ec7126d8d8
Silentiy <a.gwrlnk@gmail.com>
2022-09-13 23:46:25 +0300

    "GroupList view, group_list and base_bootstrap templates"

--- beam/autograder/views.py
+++ beam/autograder/views.py
@@ -1,6 +1,17 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
+from django.views import generic
+from .models import Group, Student
+from django.urls import reverse_lazy
 
 
 def test_view(request):
     return HttpResponse("Hello, world!")
+
+
+class GroupList(generic.ListView):
+    model = Group
+
+    template_name = "RC_beam/group_list.html"
+    context_object_name = "group_list"
+    # there is no need to specify two above variables; Django uses them by default
