df92f397906f75d186f6537a6481573d74464b38
Silentiy <a.gwrlnk@gmail.com>
2022-09-13 22:55:12 +0300

    "Group and Student models"

--- beam/autograder/models.py
+++ beam/autograder/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
-# Create your models here.
+
+class Group(models.Model):
+    group_year = models.PositiveSmallIntegerField()
+    group_name = models.CharField(
+        max_length=12)
+
+    def __str__(self):
+        return self.group_name
+
+
+class Student(models.Model):
+    full_name = models.CharField(
+        max_length=300,
+        help_text="Family name, given name, patronymic (if acceptable)"
+    )
+    group = models.ForeignKey("Group", on_delete=models.CASCADE, null=False)
+    subgroup_variant_number = models.PositiveSmallIntegerField()
+    personal_variant_number = models.PositiveSmallIntegerField()
+
+    def __str__(self):
+        return str(self.full_name) + " " + str(self.group)
