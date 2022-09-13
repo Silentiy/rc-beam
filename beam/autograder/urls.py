27d88dc0a6d3c03dbde886db3c4e4289cdaed122
Silentiy <a.gwrlnk@gmail.com>
2022-09-14 00:49:21 +0300

    "student_list and student_personal with according views and urls"

--- beam/autograder/urls.py
+++ beam/autograder/urls.py
@@ -4,4 +4,7 @@ from . import views
 app_name = 'grader'
 urlpatterns = [
     path('', views.test_view, name='home'),
+    path('groups/', views.GroupList.as_view(), name='group_list'),
+    path('groups/<int:group_id>', views.StudentList.as_view(), name='student_list'),
+    path('groups/<int:group_id>/<int:stud_id>/', views.student_personal_view, name='student_personal'),
 ]
